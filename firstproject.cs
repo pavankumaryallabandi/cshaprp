75e14228e2d5d0b8b00e0729d04e21e1023ae421
pavan newgit <pavankumary1993@gmail.com>
2023-03-28 16:40:49 +0530

    first push by hemanth

---
+++ firstproject.cs
@@ -0,0 +1,21 @@
+using System;
+namespace pavan
+{
+	class learncsharp
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("hai pavan ");
+			Console.WriteLine("hai hemanth");
+			Console.WriteLine("this is our first project lets rock ");
+
+		}
+
+
+
+
+
+	}
+
+}
